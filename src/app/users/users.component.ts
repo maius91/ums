0f88e6e11558e64e22de6233547123187da76747
felice.avitto <felice.avitto@finconsgroup.com>
2023-02-28 10:51:25 +0100

    aggiunti componenti padre e figlio

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,48 +1,22 @@
 import { style } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls : ['./users.component.css']
 })
-export class UsersComponent {
-    users = [
-        {
-            firstname: 'Felice',
-            lastname: 'Avitto',
-            email: 'felice0091@gmail.com',
-            fiscalcode: 'VTTFLC91T03E223L',
-            phone: '1234567890',
-            province: 'Bari',
-            age: 31
-        },
-        {
-            firstname: 'Felice1',
-            lastname: 'Avitto1',
-            email: 'felice0091@gmail.com',
-            fiscalcode: 'VTTFLC91T03E223L',
-            phone: '1234567890',
-            province: 'Bari',
-            age: 31
-        },
-        {
-            firstname: 'Felice2',
-            lastname: 'Avitto2',
-            email: 'felice0091@gmail.com',
-            fiscalcode: 'VTTFLC91T03E223L',
-            phone: '1234567890',
-            province: 'Bari',
-            age: 31
-        },
-        {
-            firstname: 'Felice3',
-            lastname: 'Avitto3',
-            email: 'felice0091@gmail.com',
-            fiscalcode: 'VTTFLC91T03E223L',
-            phone: '1234567890',
-            province: 'Bari',
-            age: 31
-        }
-    ]
+export class UsersComponent implements OnInit{
+    title = 'Users'
+    public users:any[] = [];
+
+    constructor(private service:UserService)
+    {
+        
+    }
+
+    ngOnInit(): void {
+      this.users = this.service.getUsers();
+    }
 }
